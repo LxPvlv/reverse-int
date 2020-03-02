cb9566b479a0a146cc15b60f1e072c320ec6550f
lxpvlv <w.lxpvlv@gmail.com>
2020-03-03 00:28:28 +0500

    feat: add solution for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = reverse = n =>
+    Number(
+        Math.abs(n)
+            .toString()
+            .split("")
+            .reverse()
+            .join("")
+    );
